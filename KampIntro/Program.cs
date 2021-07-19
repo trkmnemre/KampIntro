a5b1bedaa96851ddaba0ce4af140c21e2562783d
Admin <Admin@DESKTOP-8JAIHU6>
2021-07-19 20:27:06 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[]
+                {
+                    urun1,urun2
+                };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine("Adı : "+urun.Adi + " Fiyatı : " +urun.Fiyati + " Açıklma : "+ urun.Aciklama);
+            }
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Matematik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dortislem dortislem = new Dortislem();
+            dortislem.Topla(4,5);
+            dortislem.Topla(6,9);
+
+        }
+    }
+}
