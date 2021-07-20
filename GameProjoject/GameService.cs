d9a221d15d95f717e51882e8b0074ffc9e5ffee5
Emre Türkmen <emreturkmen1@outlook.com>
2021-07-20 15:17:41 +0300

    Engin Demiroğ 08.20 Ders Sonu

---
+++ GameProjoject/GameService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProjoject
+{
+    class GameService : IGamerService
+    {
+        IUserValidationService _userValidationService;
+
+        public GameService(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer)==true)
+            {
+                Console.WriteLine("Kayıt olundu.");
+            }
+            else
+            {
+                Console.WriteLine("Doğrulama Başarız. Kayıt Başarısız.");
+            }
+     
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Güncellendi.");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Silindi.");
+        }
+    }
+}
---
+++ GameProjoject/UserValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProjoject
+{
+    class UserValidation : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear==1985 && gamer.FirstName=="Emre"&& gamer.IdentityNumber==12345)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
